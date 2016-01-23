4af86819adc242ca4591efc3e99878773cc6067b
Sergey Sova <i.am@lestad.net>
2016-01-23 13:50:22 +0300

    Game finished

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,8 @@ fn main() {
 
   let secret_number = rand::thread_rng().gen_range(1, 101);
 
-  println!("The secret number is: {}", secret_number);
-
   loop {
-    println!("Please input your guess:");
+    println!("Input your guess:");
 
     let mut guess = String::new();
 
