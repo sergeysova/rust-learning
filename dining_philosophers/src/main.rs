4ef36931dca2ddded0a1d2df2634b5530c4d2584
Sergey Sova <i.am@lestad.net>
2016-01-23 14:00:03 +0300

    Add vector

--- dining_philosophers/src/main.rs
+++ dining_philosophers/src/main.rs
@@ -9,13 +9,23 @@ impl Philosopher {
       name: name.to_string(),
     }
   }
+
+  fn eat(&self) {
+    println!("{} is done eating.", self.name);
+  }
 }
 
 
 fn main() {
-    let p1 = Philosopher::new("Judith Butler");
-    let p2 = Philosopher::new("Gilles Deleuze");
-    let p3 = Philosopher::new("Karl Marx");
-    let p4 = Philosopher::new("Emma Goldman");
-    let p5 = Philosopher::new("Michel Foucault");
+  let philosophers = vec![
+    Philosopher::new("Judith Butler"),
+    Philosopher::new("Gilles Deleuze"),
+    Philosopher::new("Karl Marx"),
+    Philosopher::new("Emma Goldman"),
+    Philosopher::new("Michel Foucault"),
+  ];
+
+  for ph in &philosophers {
+    ph.eat();
+  }
 }
