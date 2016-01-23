3b497d2dae365cc877f39ba3bbabf9a0e21358bf
Sergey Sova <i.am@lestad.net>
2016-01-23 15:32:54 +0300

    Add array and tuple

--- function_pointers/src/main.rs
+++ function_pointers/src/main.rs
@@ -15,4 +15,35 @@ fn main() {
 
   let var = callback(plus);
   println!("{}", var);
+
+  arrays();
+  tuples();
 }
+
+fn arrays() {
+  let _a = [1, 2, 3]; // immutable array of [i32; 3]
+  let mut _b = [1, 2, 3]; // mutable array of [i32; 3]
+
+  let c = [0; 20]; // initialize 20 elements with 0
+
+  let _middle = &c[5..15]; // get the middle of array
+}
+
+
+///
+/// Simple tuple example
+///
+/// ```rust
+///   let tuple = (1, true, "hello");
+/// ```
+///
+fn tuples() {
+  let a = (1, "hello");
+  let b: (i32, &str) = a;
+
+  let (x, y) =  b;
+
+  println!("first in tuple: {}, second: {}", x, y);
+  println!("show tuple: {{{}, {}}}", b.0, b.1);
+}
+
