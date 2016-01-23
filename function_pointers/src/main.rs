63cf44866c0385c554a26ab37eb2f37de5245a9a
Sergey Sova <i.am@lestad.net>
2016-01-23 15:22:53 +0300

    Function pointers and signed valuese

--- function_pointers/src/main.rs
+++ function_pointers/src/main.rs
@@ -1,3 +1,18 @@
+
+
+fn callback(cb: fn(i32, i32) -> i32) -> i32 {
+  cb(12, 24)
+}
+
+
 fn main() {
-    println!("Hello, world!");
+  fn plus(a: i32, b: i32) -> i32 {
+    a + b
+  };
+
+  let f = plus;
+  println!("{}", f(12, -55));
+
+  let var = callback(plus);
+  println!("{}", var);
 }
